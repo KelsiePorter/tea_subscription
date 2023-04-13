e57db2054e91bdf6584d4aa4bae962dab2628bf1
Kelsie Porter <krporter22@gmail.com>
2023-04-12 19:38:30 -0600

    Add validations and assoc tests

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,6 @@ class Subscription < ApplicationRecord
   
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+
+  validates_presence_of :title, :price, :status, :frequency
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Subscription do
     it { should have_many(:subscription_teas) }
     it { should have_many(:teas).through(:subscription_teas) }
   end
+
+  describe 'validations' do 
+    it {should validate_presence_of(:title) }
+    it {should validate_presence_of(:price) }
+    it {should validate_presence_of(:status) }
+    it {should validate_presence_of(:frequency) }
+  end
 end
