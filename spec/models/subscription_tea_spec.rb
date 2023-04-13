91ef1623ae75aa09d83a4e7699145cc8237245bd
Kelsie Porter <krporter22@gmail.com>
2023-04-12 19:41:29 -0600

    Add validations and assoc tests

--- spec/models/subscription_tea_spec.rb
+++ spec/models/subscription_tea_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe SubscriptionTea do
     it { should belong_to(:tea) }
     it { should belong_to(:subscription) }
   end
+
+  describe 'validations' do 
+    it {should validate_presence_of(:subscription_id) }
+    it {should validate_presence_of(:tea_id) }
+  end
 end
