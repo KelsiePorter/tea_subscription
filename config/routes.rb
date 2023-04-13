7cb7013f09497714b9d2175afb8ffe0c208b452c
Kelsie Porter <krporter22@gmail.com>
2023-04-12 21:13:14 -0600

    Add post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do 
+    namespace :v1 do 
+      post '/customers/:customer_id/subscriptions/:subscription_id', to: 'customers#subscribe'
+    end
+  end
 end
