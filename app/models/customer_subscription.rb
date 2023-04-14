3d70c95688a558b2f896b8a472c4d7f27fe36897
Kelsie Porter <krporter22@gmail.com>
2023-04-13 18:18:04 -0600

    Create and add model test for validating subscription uniqueness

--- app/models/customer_subscription.rb
+++ app/models/customer_subscription.rb
@@ -3,4 +3,19 @@ class CustomerSubscription < ApplicationRecord
   belongs_to :subscription
 
   validates_presence_of :customer_id, :subscription_id
+
+  validate :validate_unique_subscription
+
+  private
+
+  def validate_unique_subscription
+    duplicates = CustomerSubscription.where(
+      customer_id: self.customer_id, 
+      subscription_id: self.subscription_id
+    )
+    
+    if duplicates.size > 0
+      errors.add(:customer, "is already subscribed to this tea subscription")
+    end
+  end
 end
